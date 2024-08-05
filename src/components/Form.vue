4489de52c67785a0ece3829c441110b8718df695
xwan0377 <xwan0377@student.monash.edu>
2024-08-05 13:32:25 +1000

    Week3 Activity 6

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,7 +7,7 @@
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <lable for="username" class="form-label">Username</lable>
-                            <input type="text" class="form-control" id="username" v-model="FormData.username">
+                            <input type="text" class="form-control" id="username" v-model="formData.username">
                         </div>
                         <div class="col-md-6">
                             <lable for="password" class="form-label">Password</lable>
@@ -39,18 +39,61 @@
                         <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
                     </div>
                 </form>
-
             </div>
-
         </div>
-
     </div>
+    <div class="row mt-5" v-if="submittedCards.length">
+   <div class="d-flex flex-wrap justify-content-start">
+      <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
+         <div class="card-header">
+            User Information
+         </div>
+         <ul class="list-group list-group-flush">
+            <li class="list-group-item">Username: {{ card.username }}</li>
+            <li class="list-group-item">Password: {{ card.password }}</li>
+            <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
+            <li class="list-group-item">Gender: {{ card.gender }}</li>
+            <li class="list-group-item">Reason: {{ card.reason }}</li>
+         </ul>
+      </div>
+   </div>
+</div>
 </template>
 
 <script setup>
-
+import { ref } from 'vue';
+  
+  const formData = ref({
+      username: '',
+      password: '',
+      isAustralian: false,
+      reason: '',
+      gender: ''
+  });
+  
+  const submittedCards = ref([]);
+  
+  const submitForm = () => {
+      submittedCards.value.push({
+          ...formData.value
+      });
+  };
 </script>
+<style scoped>
 
-<script setup>
+   .card {
+   border: 1px solid #ccc;
+   border-radius: 10px;
+   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+   }
+   .card-header {
+   background-color: #275FDA;
+   color: white;
+   padding: 10px;
+   border-radius: 10px 10px 0 0;
+   }
+   .list-group-item {
+   padding: 10px;
+   }
+</style>
 
-</script>
